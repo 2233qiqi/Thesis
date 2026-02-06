5cda087693a1e54e37093219faf275cb02814c28
2233qiqi <1091682015@qq.com>
2026-02-06 10:47:41 +0800

    2/6

--- src/PhysicsList.cc
+++ src/PhysicsList.cc
@@ -3,3 +3,17 @@
 #include "G4EmStandardPhysics_option4.hh"
 #include "G4DecayPhysics.hh"
 #include "G4RadioactiveDecayPhysics.hh"
+
+#include "G4StoppingPhysics.hh"
+#include "G4IonPhysics.hh"
+#include "G4NeutronTrackingCut.hh"
+
+PhysicsList :: PhysicsList()
+{
+    RegisterPhysics(new G4DecayPhysics());
+    RegisterPhysics(new G4RadioactiveDecayPhysics());
+    RegisterPhysics(new G4EmStandardPhysics_option4());
+    RegisterPhysics(new G4IonPhysics());
+    
+
+}
