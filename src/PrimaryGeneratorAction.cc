904425112f1878e6e4e9ff2329afcd5acc5301f0
2233qiqi <1091682015@qq.com>
2026-02-18 17:56:03 +0800

    2/18

---
+++ src/PrimaryGeneratorAction.cc
@@ -0,0 +1,19 @@
+#include "PrimaryGeneratorAction.hh"
+
+#include "G4ParticleGun.hh"
+#include "G4IonTable.hh"
+#include "G4ParticleDefinition.hh"
+#include "G4Geantino.hh"    
+#include "G4SystemOfUnits.hh"
+#include "G4Event.hh"
+#include "G4Exception.hh"
+
+PrimaryGeneratorAction::PrimaryGeneratorAction()
+{
+    fGPS = new G4GeneralParticleSource();
+}
+
+PrimaryGeneratorAction::~PrimaryGeneratorAction()
+{
+    
+}
