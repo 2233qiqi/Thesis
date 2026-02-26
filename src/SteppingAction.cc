41db0bbaaba3cf231cf2478ad036b81e5b70555e
2233qiqi <1091682015@qq.com>
2026-02-26 15:41:13 +0800

    2/26

--- src/SteppingAction.cc
+++ src/SteppingAction.cc
@@ -1,3 +1,8 @@
 #include "EventAction.hh"
 #include "SteppingAction.hh"
 #include "G4Step.hh"
+
+SteppingAction :: SteppingAction(EventAction* eventAction)
+: G4UserSteppingAction(), fEventAction(eventAction)
+{
+}
