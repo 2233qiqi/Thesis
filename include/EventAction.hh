98db616796a01662a02acffe64e1337ccf1c158a
2233qiqi <1091682015@qq.com>
2026-02-26 09:17:13 +0800

    2/26

---
+++ include/EventAction.hh
@@ -0,0 +1,30 @@
+#ifndef EventAction_h
+#define EventAction_h 1
+
+#include "G4UserEventAction.hh"
+#include "globals.hh"
+#include <vector>
+
+class EvenAction : public G4UserEventAction
+{
+    public: 
+    EvenAction();
+    virtual ~EvenAction();
+
+    virtual void BeginOfEventAction(const G4Event* anEvent);
+    virtual void EndOfEventAction(const G4Event* anEvent);
+    void AddEnergy(G4int crystalID,G4double edep)
+    [
+        
+    ]
+
+
+    private:
+
+
+}
+
+
+
+
+#endif
