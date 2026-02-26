8254df7bb9e42a6c0b0878d3dc043c25b5e19fa8
2233qiqi <1091682015@qq.com>
2026-02-26 13:53:02 +0800

    02/26

--- src/RunAction.cc
+++ src/RunAction.cc
@@ -2,3 +2,9 @@
 #include "G4Run.hh"
 #include "G4SystemOfUnits.hh"
 #include "G4AnalysisManager.hh"
+
+RunAction :: RunAction() : G4UserRunAction()
+{
+    auto analysisMannager = G4AnalysisManager::Instance();
+    analysisMannager->SetDefaultFileType("root");
+}
