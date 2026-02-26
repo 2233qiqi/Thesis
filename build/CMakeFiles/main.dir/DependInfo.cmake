d356f7b5f3ea416bcb3fef49d7c1ef7eb1712ff1
2233qiqi <1091682015@qq.com>
2026-02-26 16:47:43 +0800

    2/26

---
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/qiqi/geant4/Thesis/main.cc" "CMakeFiles/main.dir/main.cc.o" "gcc" "CMakeFiles/main.dir/main.cc.o.d"
+  "/home/qiqi/geant4/Thesis/src/ActionInitialization.cc" "CMakeFiles/main.dir/src/ActionInitialization.cc.o" "gcc" "CMakeFiles/main.dir/src/ActionInitialization.cc.o.d"
+  "/home/qiqi/geant4/Thesis/src/DetectorConstruction.cc" "CMakeFiles/main.dir/src/DetectorConstruction.cc.o" "gcc" "CMakeFiles/main.dir/src/DetectorConstruction.cc.o.d"
+  "/home/qiqi/geant4/Thesis/src/EventAction.cc" "CMakeFiles/main.dir/src/EventAction.cc.o" "gcc" "CMakeFiles/main.dir/src/EventAction.cc.o.d"
+  "/home/qiqi/geant4/Thesis/src/PhysicsList.cc" "CMakeFiles/main.dir/src/PhysicsList.cc.o" "gcc" "CMakeFiles/main.dir/src/PhysicsList.cc.o.d"
+  "/home/qiqi/geant4/Thesis/src/PrimaryGeneratorAction.cc" "CMakeFiles/main.dir/src/PrimaryGeneratorAction.cc.o" "gcc" "CMakeFiles/main.dir/src/PrimaryGeneratorAction.cc.o.d"
+  "/home/qiqi/geant4/Thesis/src/RunAction.cc" "CMakeFiles/main.dir/src/RunAction.cc.o" "gcc" "CMakeFiles/main.dir/src/RunAction.cc.o.d"
+  "/home/qiqi/geant4/Thesis/src/SteppingAction.cc" "CMakeFiles/main.dir/src/SteppingAction.cc.o" "gcc" "CMakeFiles/main.dir/src/SteppingAction.cc.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
